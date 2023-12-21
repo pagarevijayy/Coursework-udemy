b076be3314be48d8ae003cb09e3827ce2213bb4b
pagarevijayy <pagarevj97@gmail.com>
2023-12-21 14:01:02 +0530

    express: cleaner way to serve files with dynamic path

--- nodejs-complete-guide/express-server/routes/admin.js
+++ nodejs-complete-guide/express-server/routes/admin.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => { 
-    res.status(200).sendFile(path.join(__dirname, '..', '/views/add-product.html'))
+    res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res, next) => { 
