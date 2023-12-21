a7a31d70333647a58cdc026110dbc6269362cd84
pagarevijayy <pagarevj97@gmail.com>
2023-12-21 14:15:31 +0530

    prettier installed + html pages styled a bit

--- nodejs-complete-guide/express-server/app.js
+++ nodejs-complete-guide/express-server/app.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const adminRouter = require('./routes/admin')
-const shopRouter = require('./routes/shop')
-
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false})) //next() is implicitly called by that inner function.
+app.use(bodyParser.urlencoded({ extended: false })); //next() is implicitly called by that inner function.
 
 //order of middleware matters.
-app.use('/admin',adminRouter);
+app.use("/admin", adminRouter);
 app.use(shopRouter);
 
 // generic 404 error handling for route not found
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
-})
+    res.status(404).send("<h1>Page not found</h1>");
+});
 
-app.listen(3000)
+app.listen(3000);
--- nodejs-complete-guide/express-server/routes/shop.js
+++ nodejs-complete-guide/express-server/routes/shop.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => { 
+router.get("/", (req, res, next) => {
     // res.status(200).send('<h1>Hello from Poke Shop!</h1>');
-    res.status(200).sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.status(200).sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
