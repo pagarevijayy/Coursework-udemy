a3694c460bff148b0a9f723e0aec63a781ea4c64
pagarevijayy <pagarevj97@gmail.com>
2023-12-21 13:51:37 +0530

    express - html pages served w/ dynamic path

--- nodejs-complete-guide/express-server/routes/admin.js
+++ nodejs-complete-guide/express-server/routes/admin.js
@@ -1,15 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => { 
-    res.status(200).send(`
-        <h1>Add a new product!</h1>
-        <form method='POST' action='/admin/add-product'>
-            <input type='text' name='product'/>
-            <button type='submit'>Submit</button>
-        </form>
-    `);
+    res.status(200).sendFile(path.join(__dirname, '..', '/views/add-product.html'))
 });
 
 router.post('/add-product', (req, res, next) => { 
--- nodejs-complete-guide/express-server/routes/shop.js
+++ nodejs-complete-guide/express-server/routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => { 
-    res.status(200).send('<h1>Hello from Poke Shop!</h1>');
+    // res.status(200).send('<h1>Hello from Poke Shop!</h1>');
+    res.status(200).sendFile(path.join(__dirname, '..' ,'/views/shop.html'));
 });
 
 module.exports = router;
