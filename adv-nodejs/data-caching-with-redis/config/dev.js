3095b4786c3b1efc18ce045192f1f1323ff0d816
pagarevijayy <pagarevj97@gmail.com>
2023-12-27 19:52:14 +0530

    key update

--- adv-nodejs/data-caching-with-redis/config/dev.js
+++ adv-nodejs/data-caching-with-redis/config/dev.js
@@ -2,6 +2,9 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb+srv://captcaptvj:5IdUqN5rr9wWBGS1@learning-poc.q0zoglm.mongodb.net/advnode?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://username:5IdUqN5rr9wWBGS1@learning-poc.q0zoglm.mongodb.net/advnode?retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
+
+
+//captain vj
