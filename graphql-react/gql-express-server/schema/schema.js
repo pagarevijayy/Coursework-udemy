a1741becd06070df675131639d4e230c8124778d
pagarevijayy <pagarevj97@gmail.com>
2024-01-02 13:15:49 +0530

    axios integration for data from external server

--- graphql-react/gql-express-server/schema/schema.js
+++ graphql-react/gql-express-server/schema/schema.js
@@ -1,11 +1,6 @@
 const graphql = require("graphql");
-const _ = require("lodash");
+const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-    { id: "12", firstName: "Bill", age: 23 },
-    { id: "23", firstName: "Vijay", age: 25 },
-];
 
 const UserType = new GraphQLObjectType({
     name: "User",
@@ -23,7 +18,10 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                // resolve has the logic to return data - can be from db, third-party/external server
+                return axios
+                    .get(`http://localhost:3000/users/${args.id}`)
+                    .then((res) => res.data);
             },
         },
     },
